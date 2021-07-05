c796292028d6bd407e2aca8f5447abd2431c6285
Dinnonix <47343418+Dinnonix@users.noreply.github.com>
2021-07-06 10:56:09 +1200

    Summary

--- Player/Reaper/Controls/PlayerReaper.gd
+++ Player/Reaper/Controls/PlayerReaper.gd
@@ -8,27 +8,36 @@ var move_speed = 5 * 40
 var gravity = 1200
 var jump_velocity = -500
 var is_grounded 
+#var is_dead = false
 
 onready var animator = $Body/AnimatedReaper
 onready var raycasts = $Raycasts
 
 func _physics_process(delta):
-	var move = _get_input()
-	velocity.y += gravity * delta 
-	velocity = move_and_slide(velocity, Vector2.UP, true)
-	is_grounded = _check_is_grounded()
-	if not is_grounded:
-		animator.play("Jump")
-	elif move != 0:
-		$Body.scale.x = move
-		animator.play("Run")
-	elif move == 0:
-		animator.play("Idle")
+	#if is_dead == false:
+		var move = _get_input()
+		velocity.y += gravity * delta 
+		velocity = move_and_slide(velocity, Vector2.UP, true)
+		is_grounded = _check_is_grounded()
+		if not is_grounded:
+			animator.play("Jump")
+		elif move != 0:
+			$Body.scale.x = move
+			animator.play("Run")
+		elif move == 0:
+			animator.play("Idle")
+		if Input.is_action_pressed("jump") && is_grounded: 
+			velocity.y = jump_velocity 
+			animator.play("Jump")
+		if Input.is_action_pressed("attack") && is_grounded:
+			animator.play("Attack")
 
-func _input(event):
-	if event.is_action_pressed("jump") && is_grounded: 
-		velocity.y = jump_velocity 
-		animator.play("Jump")
+#func dead():
+#	is_dead = true
+#	velocity = Vector2(0,0)
+#	animator.play("Dead")
+#	$ReaperCollision.disabled = true
+#	$Timer.start()
 
 func _get_input():
 	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
