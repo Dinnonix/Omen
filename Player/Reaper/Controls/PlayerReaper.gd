233b2cc06412d579a1da3e71bbbcfe17451bd2bf
Dinnonix <47343418+Dinnonix@users.noreply.github.com>
2021-09-17 12:17:04 +1200

    For Home

--- Player/Reaper/Controls/PlayerReaper.gd
+++ Player/Reaper/Controls/PlayerReaper.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const SLOPE_STOP = 64
-
+var move 
 var velocity = Vector2()
 var move_speed = 5 * 40
 var gravity = 1200
@@ -13,25 +13,59 @@ var isAttacking = false
 
 onready var animator = $Body/AnimationTree.get("parameters/playback")
 onready var raycasts = $Raycasts
-
+func attack():
+	animator.travel("attack");
+	
+	isAttacking = true
+	
+func end_attack():
+	isAttacking = false
+	
+		
 func _physics_process(delta):
-	if is_dead == false && isAttacking == false:
-		var move = _get_input()
-		velocity.y += gravity * delta 
-		velocity = move_and_slide(velocity, Vector2.UP, true)
+
+	if is_dead == false:
+		
+	
 		is_grounded = _check_is_grounded()
-		if Input.is_action_pressed("jump") && is_grounded: 
-			velocity.y = jump_velocity 
-			animator.travel("jump") 
-		if not is_grounded:
-			animator.travel("fall") 
-		elif move != 0:
-			$Body.scale.x = move
-			animator.travel("running")
-		if Input.is_action_just_pressed("attack"):
-			animator.travel("attack");
-		elif move == 0:
-			animator.travel("idle")
+		if not isAttacking:
+			move = _get_input()
+			
+			if Input.is_action_pressed("jump") && is_grounded:
+				velocity.y = jump_velocity
+				animator.travel("jump")
+			elif not is_grounded:
+				if velocity.y > 0:
+					animator.travel("fall")
+				elif velocity.y < 0:
+					animator.travel("jump")
+			elif move != 0:
+				$Body.scale.x = move
+				animator.travel("running")
+			elif move == 0:
+				animator.travel("idle")
+			
+			if Input.is_action_just_pressed("attack"):
+				attack()
+		if isAttacking:
+			if velocity.y != 0:
+				velocity.x = move_toward(velocity.x,0, 3)
+			else:
+				velocity.x = move_toward(velocity.x,0, 10)
+		velocity.y += gravity * delta 
+		velocity = move_and_slide(velocity, Vector2.UP, true)	
+#		if Input.is_action_pressed("jump") && is_grounded: 
+#			velocity.y = jump_velocity 
+#			animator.travel("jump") 
+#		if not is_grounded:
+#			animator.travel("jump") 
+#		elif move != 0:
+#			$Body.scale.x = move
+#			animator.travel("running")
+#		if Input.is_action_just_pressed("attack"):
+#			animator.travel("attack");
+#		elif move == 0:
+#			animator.travel("idle")
 
 
 
